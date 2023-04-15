9275560dcedad076d9154e92144cf1415a535309
madhursharma026 <105696243+madhursharma026@users.noreply.github.com>
2023-04-15 21:01:32 +0530

    Updated!

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -26,7 +26,10 @@ function Header() {
     })
     return (
         <div className={`fixed-top px-4 py-2 ${visible ? styles.animationStyle1 : styles.animationStyle2}`}>
-            <Navbar expand="lg" className={`container-xl ${styles.headerBeforeLGScrn}`}>
+                <div className={`text-center ${styles.headerBeforeLGScrn} mt-1`}>
+                <input type="text" class="form-control" placeholder='Search Token and NFTs Collection' style={{maxWidth:'350px', margin:'auto', background:'transparent', border:'1px solid #ffa6d6'}} />
+                </div>
+            <Navbar expand="lg" className={`${styles.headerBeforeLGScrn}`} style={{marginTop:'-50px'}}>
                 <Link href="/">
                     <Navbar.Brand>
                         <img src="https://seeklogo.com/images/U/uniswap-logo-782F5E6363-seeklogo.com.png" alt="#ImgNotFound" width="30px" height='30px' />
@@ -40,7 +43,7 @@ function Header() {
                         <Link href="/" className={styles.headerNavbarLinks}>NFTs</Link>
                         <Link href="/" className={styles.headerNavbarLinks}>Pools</Link>
                     </Nav>
-                    <button type="button" className={`btn`} style={{ background: '#F7DDED', color: '#F31D92' }}><b>Connect</b></button>
+                    <button type="button" className={`btn mt-2`} style={{ background: '#F7DDED', color: '#F31D92' }}><b>Connect</b></button>
                 </Navbar.Collapse>
             </Navbar>
 
@@ -51,9 +54,10 @@ function Header() {
                     </Navbar.Brand>
                 </Link>
                 <div style={{ float: 'right' }}>
+                    <i class="fa fa-search" style={{ fontSize: '20px', marginRight: '20px' }}></i>
                     <button type="button" className={`btn`} style={{ background: '#F7DDED', color: '#F31D92' }}><b>Connect</b></button>
                 </div>
-                <div className="fixed-bottom p-3" style={{background:'white'}}>
+                <div className="fixed-bottom p-3" style={{ background: 'white' }}>
                     <div className="row">
                         <div className="col-3">
                             <Link href="/" className={styles.headerNavbarLinks}>Swap</Link>
--- pages/index.js
+++ pages/index.js
@@ -29,10 +29,10 @@ export default function Home() {
             </div>
           </div>
 
-          <Accordion defaultActiveKey={['0', '1']} alwaysOpen className='mt-5'>
+          {/* <Accordion defaultActiveKey={['0', '1']} alwaysOpen className='mt-5'>
             <Accordion.Item eventKey="0">
               <Accordion.Header><span style={{ fontSize: '30px', fontWeight: '600', color: 'black' }}>Swap Token</span></Accordion.Header>
-              <Accordion.Body className='p-0 m-0'>
+              <Accordion.Body className='p-0 m-0' style={{ background: 'url(https://app.uniswap.org/static/media/swapCard.1a0376fd.png) right center / auto 100% no-repeat border-box white' }}>
                 <div className='p-4'>
                   <h3 className='mb-5'>Buy, sell, and explore tokens on Ethereum, Polygon, Optimism, and more.</h3>
                   <Link href="/" style={{ color: '#FF28D4', fontSize: '25px', textDecoration: 'none' }}>Trade Tokens</Link>
@@ -41,39 +41,60 @@ export default function Home() {
             </Accordion.Item>
             <Accordion.Item eventKey="1">
               <Accordion.Header><span style={{ fontSize: '30px', fontWeight: '600', color: 'black' }}>Trade NFTs</span></Accordion.Header>
-              <Accordion.Body className='p-0 m-0'>
+              <Accordion.Body className='p-0 m-0' style={{ background: 'url(https://app.uniswap.org/static/media/nftCard.ec17cbfb.png) right center / auto 100% no-repeat border-box white' }}>
                 <div className='p-4'>
                   <h3 className='mb-5'>Buy and sell NFTs across marketplaces to find more listings at better prices.</h3>
                   <Link href="/" style={{ color: '#FF28D4', fontSize: '25px', textDecoration: 'none' }}>Explore NFTs</Link>
                 </div>
               </Accordion.Body>
             </Accordion.Item>
-          </Accordion>
+          </Accordion> */}
+
+          <div className="row mt-5">
+            <div className="col-lg-6 mt-3">
+              <div class="card p-3 h-100" style={{ background: 'url(https://app.uniswap.org/static/media/swapCard.1a0376fd.png) right center / auto 100% no-repeat border-box white', boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px", border: '0', borderRadius:'30px' }}>
+                <div class="card-body">
+                  <h2 class="card-title">Swap Token</h2>
+                  <h5 class="card-subtitle mb-2 mt-5 pt-5 text-secondary">Buy, sell, and explore tokens on Ethereum, Polygon, Optimism, and more.</h5>
+                  <Link href="/" style={{ color: '#FF28D4', fontSize: '25px', textDecoration: 'none' }}>Trade Tokens</Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 mt-3">
+              <div class="card p-3 h-100" style={{ background: 'url(https://app.uniswap.org/static/media/nftCard.ec17cbfb.png) right center / auto 100% no-repeat border-box white', boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px", border: '0', borderRadius:'30px' }}>
+                <div class="card-body">
+                  <h2 class="card-title">Trade NFTs</h2>
+                  <h5 class="card-subtitle mb-2 mt-5 pt-5 text-secondary">Buy and sell NFTs across marketplaces to find more listings at better prices.</h5>
+                  <Link href="/" style={{ color: '#FF28D4', fontSize: '25px', textDecoration: 'none' }}>Explore NFTs</Link>
+                </div>
+              </div>
+            </div>
+          </div>
 
           <div className="row mt-5">
             <div className="col-lg-4 mt-lg-0 mt-3">
-              <div class="card p-3 h-100" style={{ background: '#F5F6FC', border: 'none', boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px" }}>
+              <div class="card p-3 h-100" style={{ background: '#F5F6FC', border: 'none', boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px", borderRadius:'30px' }}>
                 <div class="card-body">
-                  <h2 class="card-title">Buy Crypto <span style={{ float: 'right' }}><i class="fa fa-dollar"></i></span></h2>
-                  <h4 class="card-subtitle mb-2 mt-4 text-secondary">Buy crypto with your credit card or bank account at the best rates.</h4>
+                  <h2 class="card-title">Buy Crypto <span style={{ float: 'right', color: '#6C757D' }}><i class="fa fa-dollar"></i></span></h2>
+                  <h4 class="card-subtitle my-4 text-secondary">Buy crypto with your credit card or bank account at the best rates.</h4>
                   <Link href="/" style={{ color: '#FF28D4', fontSize: '20px', textDecoration: 'none' }}>Buy now</Link>
                 </div>
               </div>
             </div>
             <div className="col-lg-4 mt-lg-0 mt-3">
-              <div class="card p-3 h-100" style={{ background: '#F5F6FC', border: 'none', boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px" }}>
+              <div class="card p-3 h-100" style={{ background: '#F5F6FC', border: 'none', boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px", borderRadius:'30px' }}>
                 <div class="card-body">
-                  <h2 class="card-title">Earn <span style={{ float: 'right' }}><i class="fa fa-line-chart"></i></span></h2>
-                  <h4 class="card-subtitle mb-2 mt-4 text-secondary">Provide liquidity to pools on Uniswap and earn fees on swaps.</h4>
+                  <h2 class="card-title">Earn <span style={{ float: 'right', color: '#6C757D' }}><i class="fa fa-line-chart"></i></span></h2>
+                  <h4 class="card-subtitle my-4 text-secondary">Provide liquidity to pools on Uniswap and earn fees on swaps.</h4>
                   <Link href="/" style={{ color: '#FF28D4', fontSize: '20px', textDecoration: 'none' }}>Provide liquidity</Link>
                 </div>
               </div>
             </div>
             <div className="col-lg-4 mt-lg-0 mt-3">
-              <div class="card p-3 h-100" style={{ background: '#F5F6FC', border: 'none', boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px" }}>
+              <div class="card p-3 h-100" style={{ background: '#F5F6FC', border: 'none', boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px", borderRadius:'30px' }}>
                 <div class="card-body">
-                  <h2 class="card-title">Build dApps <span style={{ float: 'right' }}><i class="fa fa-angle-double-right"></i></span></h2>
-                  <h4 class="card-subtitle mb-2 mt-4 text-secondary">Build apps and tools on the largest DeFi protocol on Ethereum.</h4>
+                  <h2 class="card-title">Build dApps <span style={{ float: 'right', color: '#6C757D' }}><i class="fa fa-angle-double-right"></i></span></h2>
+                  <h4 class="card-subtitle my-4 text-secondary">Build apps and tools on the largest DeFi protocol on Ethereum.</h4>
                   <Link href="/" style={{ color: '#FF28D4', fontSize: '20px', textDecoration: 'none' }}>Developer Docs</Link>
                 </div>
               </div>
@@ -87,7 +108,7 @@ export default function Home() {
                 <h5>The leading decentralized crypto trading protocol, governed by a global community.</h5>
               </div>
               <div className="col-lg-3 mt-lg-2 mt-5">
-              <button type="button" className={`btn px-5 py-2`} style={{border:'1px solid white', color:'white', borderRadius:'20px'}}><b>Learn More</b></button>
+                <button type="button" className={`btn px-5 py-2`} style={{ border: '1px solid white', color: 'white', borderRadius: '20px' }}><b>Learn More</b></button>
               </div>
             </div>
           </div>
